c112ea0849615ce43853d77b79ac7f6fa124ba1a
= <bekuzarovchermen@gmail.com>
2025-03-21 00:20:44 +0300

    Commit: added jwt token for sign up

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -1,11 +1,19 @@
 import User from "../models/user.js";
-
+import jsonwebtoken from 'jsonwebtoken';
 class Auth_controller{
+    static sign_jwt(id){
+        return jsonwebtoken.sign({id: id}, process.env.JWT_SECRET_KEY, {
+            expiresIn: process.env.JWT_EXPIRES
+        });
+    }
     async sign_up(req, res, next) {
         try{
-            let user = await User.insertOne(req.body);
+            let user = await User.create(req.body);
+            let token = Auth_controller.sign_jwt(user._id);
+            
             res.status(201).json({
                 message: 'success',
+                token
             })
         }catch(e){
             res.status(400).json({
