2b414fd391643b2576bd71d1fba3de06e965c8f5
= <bekuzarovchermen@gmail.com>
2025-03-20 14:34:42 +0300

    connected db

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
-import app from './app.js'
+import app from './app.js';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
+dotenv.config({path: './main.env'});
 
+mongoose.connect(process.env.DB_URL).then(value => {
+    console.log('db connected')
+}).catch((reason) => console.log(reason));
 
 
 const PORT = 8000
