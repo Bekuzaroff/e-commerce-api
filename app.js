3caebee21958c0e2a2eac14491a96bcc94f52a68
= <bekuzarovchermen@gmail.com>
2025-03-20 14:17:32 +0300

    connected mongo db, added json middleware and added auth router for auth nodes

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ import express from 'express';
 const app = express()
 
 
+//global middlewares
+app.use(express.json());
+
 
 export default app;
+
+
+
+
 
 
 
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
-import app from './app.js'
+import app from './app.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 
+dotenv.config({path: './main.env'});
 
+mongoose.connect(process.env.DB_URL).then((value) => {
+    console.log('db is connected');
+})
 
 const PORT = 8000
 app.listen(PORT, '127.0.0.1', () => {
-    console.log('start server')
+    console.log('start server');
 })
