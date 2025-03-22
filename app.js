1875a017e1ad703509ff6f11f7900731262b27bf
= <bekuzarovchermen@gmail.com>
2025-03-22 18:38:24 +0300

    Commit: security - rate limiting

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = require('./routers/auth_router');
+const limiter = require('express-rate-limit');
 
 const app = express();
 
 let error_handle = (err, req, res, next) => {
-    console.log(err);
     res.status(err.statusCode).json({
         status: err.status,
         message: err.message
@@ -13,6 +13,11 @@ let error_handle = (err, req, res, next) => {
 }
 
 app.use(express.json());
+app.use(limiter({
+    max: 3,
+    windowMs: 60 * 60 * 1000,
+    message: 'too many calls from this ip address, try again later'
+}))
 app.use('/api/v1/auth', router);
 app.use(error_handle);
 
