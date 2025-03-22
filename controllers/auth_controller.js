933c2a86deabbf5c24b1dc1e7ed1bc4e4721484e
= <bekuzarovchermen@gmail.com>
2025-03-22 14:07:35 +0300

    Commit: jwt cookie transportation

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -2,6 +2,7 @@ const User = require('./../models/user');
 const jsonwebtoken = require('jsonwebtoken');
 const util = require('util');
 const CustomError = require('.././utils/custom_error');
+
 class Auth_controller{
     static sign_jwt(id){
         return jsonwebtoken.sign({id: id}, process.env.JWT_SECRET_KEY, {
@@ -11,6 +12,10 @@ class Auth_controller{
     async sign_up(req, res, next) {
         let user = await User.create(req.body);
         let token = Auth_controller.sign_jwt(user._id);
+
+        res.cookie('jwt', token, {
+            maxAge: process.env.JWT_EXPIRES,
+        });
 
         res.status(201).json({
             status: 'success',
@@ -34,6 +39,10 @@ class Auth_controller{
 
         let token = Auth_controller.sign_jwt(user._id);
 
+        res.cookie('jwt', token, {
+            maxAge: process.env.JWT_EXPIRES,
+        });
+
         res.status(201).json({
             status: 'success',
             token
@@ -46,8 +55,7 @@ class Auth_controller{
                 let err = new CustomError('you have not provided jwt token', 400);
                 return next(err);
             }
-           
-    
+
             const token = await util.promisify(jsonwebtoken.verify)(req.headers.jwt, process.env.JWT_SECRET_KEY);
             const id = token.id;
     
@@ -56,17 +64,16 @@ class Auth_controller{
                 return next(err);
             }
     
-            let user = await User.findByIdAndUpdate(id, req. body);
+            await User.updateOne({_id: id}, req.body);
     
             res.status(200).json({
                 status: 'success',
-                data: req.body
+                data: await User.findOne({_id: id})
             })
         }catch(e){
             let err = new CustomError(e.message, 400);
             return next(err);
         }
     }
-    
 }
 module.exports = Auth_controller;
