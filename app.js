ff17b243ef8a5fcf52570f071c8d8530fc1d7ec8
= <bekuzarovchermen@gmail.com>
2025-03-23 14:39:33 +0300

    security headers

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = require('./routers/auth_router');
 const limiter = require('express-rate-limit');
 const hpp = require('hpp');
+const helmet = require('helmet');
 
 const app = express();
 
@@ -13,13 +14,17 @@ let error_handle = (err, req, res, next) => {
     next();
 }
 
-app.use(express.json());
+//security
+app.use(helmet());
 app.use(limiter({
     max: 3,
     windowMs: 60 * 60 * 1000,
     message: 'too many calls from this ip address, try again later'
 }));
 app.use(hpp());
+
+app.use(express.json({limit: '10kb'}));
+
 
 app.use('/api/v1/auth', router);
 app.use(error_handle);
