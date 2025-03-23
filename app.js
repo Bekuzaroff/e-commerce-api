122c3f53d69d7ca39e61aeecebba628161d54396
= <bekuzarovchermen@gmail.com>
2025-03-23 16:40:25 +0300

    change error handle

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = require('./routers/auth_router');
+const limiter = require('express-rate-limit');
+const hpp = require('hpp');
 
 const app = express();
 
 let error_handle = (err, req, res, next) => {
-    console.log(err);
     res.status(err.statusCode).json({
         status: err.status,
         message: err.message
@@ -13,6 +14,13 @@ let error_handle = (err, req, res, next) => {
 }
 
 app.use(express.json());
+app.use(limiter({
+    max: 3,
+    windowMs: 60 * 60 * 1000,
+    message: 'too many calls from this ip address, try again later'
+}));
+app.use(hpp());
+
 app.use('/api/v1/auth', router);
 app.use(error_handle);
 
