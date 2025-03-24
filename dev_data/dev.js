a2525b6e38cc6680602f495f08b015e0d2725a5c
= <bekuzarovchermen@gmail.com>
2025-03-24 14:32:26 +0300

    Commit: dev.js setup db and server

---
+++ dev_data/dev.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+
+dotenv.config({path: './main.env'});
+
+mongoose.connect(process.env.DB_URL).then(value => {
+    console.log('db is connected');
+}).catch((reason) => console.log(reason));
+
+
+const PORT = 8000
+app.listen(PORT, '127.0.0.1', () => {
+    console.log('start server')
+});
